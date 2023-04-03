f0c0e6215e30842e4994502434dacd9ea861eb8c
premvinodh <premvinodh.pc@gmail.com>
2023-04-03 23:34:15 +0530

    Initial Project files commit

---
+++ src/main/java/com/playgroup/my/hibernate_training/HibernateTrainingApp.java
@@ -0,0 +1,13 @@
+package com.playgroup.my.hibernate_training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
