11bb1c7731a290d92b88d8c0d096607f192df0de
premvinodh <premvinodh.pc@gmail.com>
2023-04-06 00:46:26 +0530

    Associations - Many To Many mapping

---
+++ src/main/java/com/playgroup/my/hibernate_training/aak/mappings/manytomany/Address.java
@@ -0,0 +1,77 @@
+package com.playgroup.my.hibernate_training.aak.mappings.manytomany;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String street;
+
+	private String city;
+
+	private String state;
+
+	private String pincode;
+
+	@ManyToMany
+	private Collection<Customer> customers = new ArrayList<Customer>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+	public Collection<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(Collection<Customer> customers) {
+		this.customers = customers;
+	}
+}
