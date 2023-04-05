306eb1703bdf66607a23c6548c559b541200886d
premvinodh <premvinodh.pc@gmail.com>
2023-04-06 01:05:54 +0530

    Inheritance - Joined Table Strategy

---
+++ src/main/java/com/playgroup/my/hibernate_training/aao/inheritance/joinedstrategy/IrregularCustomer.java
@@ -0,0 +1,17 @@
+package com.playgroup.my.hibernate_training.aao.inheritance.joinedstrategy;
+
+import javax.persistence.Entity;
+
+@Entity
+public class IrregularCustomer extends Customer {
+
+	private int discountPricing;
+
+	public int getDiscountPricing() {
+		return discountPricing;
+	}
+
+	public void setDiscountPricing(int discountPricing) {
+		this.discountPricing = discountPricing;
+	}
+}
