90368f71b72c437035546ebf57f911d329561873
premvinodh <premvinodh.pc@gmail.com>
2023-04-06 00:29:04 +0530

    Associations - One To Many mapping

---
+++ src/main/java/com/playgroup/my/hibernate_training/aah/mappings/onetomany/Customer.java
@@ -0,0 +1,71 @@
+package com.playgroup.my.hibernate_training.aah.mappings.onetomany;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER")
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	private String email;
+
+	@OneToMany
+	private Collection<Address> addresses = new ArrayList<Address>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Collection<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(Collection<Address> addresses) {
+		this.addresses = addresses;
+	}
+}
---
+++ src/main/java/com/playgroup/my/hibernate_training/aah/mappings/onetomany/HibernateTest.java
@@ -0,0 +1,45 @@
+package com.playgroup.my.hibernate_training.aah.mappings.onetomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = factory.openSession();
+		session.beginTransaction();
+
+		Address addresss1 = getAddress("Street 1", "City 1", "State 1", "530001");
+		Address addresss2 = getAddress("Street 2", "City 2", "State 2", "530002");
+		
+		Customer customer = getCustomer();
+		customer.getAddresses().add(addresss1);
+		customer.getAddresses().add(addresss2);
+
+		session.save(customer);
+		session.save(addresss1);
+		session.save(addresss2);
+
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	private static Address getAddress(String street, String city, String state, String zipCode) {
+		Address addr1 = new Address();
+		addr1.setStreet(street);
+		addr1.setCity(city);
+		addr1.setState(state);
+		addr1.setPincode(zipCode);
+		return addr1;
+	}
+
+	private static Customer getCustomer() {
+		Customer customer = new Customer();
+		customer.setFirstName("Huey");
+		customer.setLastName("Duck");
+		customer.setEmail("huey.duck@gmail.com");
+		return customer;
+	}
+}
