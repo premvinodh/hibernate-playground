27d2d28d22aeed3abf1bcdbfe76262912a7d673c
premvinodh <premvinodh.pc@gmail.com>
2023-04-06 00:35:27 +0530

    Associations - One To Many mapping - @JoinTable

---
+++ src/main/java/com/playgroup/my/hibernate_training/aai/mappings/onetomany/jointable/Customer.java
@@ -0,0 +1,78 @@
+package com.playgroup.my.hibernate_training.aai.mappings.onetomany.jointable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER")
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	private String email;
+
+	@OneToMany
+	@JoinTable(
+			name="CUST_ADDR",
+			joinColumns = @JoinColumn(name = "CUST_ID"),
+			inverseJoinColumns = @JoinColumn(name = "ADDR_ID")
+	)
+	private Collection<Address> addresses = new ArrayList<Address>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Collection<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(Collection<Address> addresses) {
+		this.addresses = addresses;
+	}
+}
